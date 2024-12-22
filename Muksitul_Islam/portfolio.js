8f15ebfd2442ab188754268c6787f063f91d1270
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2024-12-22 22:43:08 +0600

    new

---
+++ Muksitul_Islam/portfolio.js
@@ -0,0 +1,8 @@
+  /* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText",{
+    strings : ["Researcher","Quick Learner","Good in SEO","Font-end Devoloper","Designer","Self Motivator"],
+    loop : true,
+    typeSpeed : 50, 
+    backSpeed : 30,
+    backDelay : 2000
+  })
